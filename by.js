d13454e2d2f9ef1e2c6f4db881811671c4a246ff
LemonIsCoding <145255975+LemonIsCoding@users.noreply.github.com>
2023-09-17 14:55:40 +0530

    Update by.js

--- by.js
+++ by.js
@@ -1,1 +1,3 @@
-const bye = ["Bye", "See you", "Goodbye", "Have a nice day", "See you later"];
+const bye = ["Bye", "See you", "Goodbye", "Have a nice day", "See you later", 
+            "random", "chatgpt"
+            ];
